47a6ed829c4c2aef4165efdd49ab8e33f79893b4
GiacomoDiNardo <vest_95@hotmail.it>
2022-06-09 15:18:37 +0200

    add html elements, add js elements

---
+++ main.js
@@ -0,0 +1,9 @@
+const appVue = new Vue ({
+    el: "#app",
+
+    data: {
+        h1Text: "Hello Vue.js",
+        image: "https://picsum.photos/200/300",
+        classList: "text-center pb-3"
+    }
+})
